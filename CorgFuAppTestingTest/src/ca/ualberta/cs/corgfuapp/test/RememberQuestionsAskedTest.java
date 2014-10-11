939dab18c387434151921881ef06c80f3c1b5742
wrflemin <wrflemin@ualberta.ca>
2014-10-11 13:41:07 -0600

    Added test cases for use cases 14-17. Also edited Controllers to make testcases compile.

--- CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/RememberQuestionsAskedTest.java
+++ CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/RememberQuestionsAskedTest.java
@@ -1,8 +1,33 @@
 package ca.ualberta.cs.corgfuapp.test;
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
+import ca.ualberta.cs.corgFuControllers.OfflineDataController;
+import ca.ualberta.cs.corgFuControllers.QAController;
+import ca.ualberta.cs.corgFuModels.OfflineData;
+import ca.ualberta.cs.corgFuModels.Question;
 
-public class RememberQuestionsAskedTest extends
-		TestCase {
-
+public class RememberQuestionsAskedTest extends TestCase {
+	
+	public RememberQuestionsAskedTest(){
+		super();
+	}
+	
+	public void testRememberAskedQs(){
+		Question mQ1 = new Question("Question to remember");
+		
+		QAController mQAC = new QAController(mQ1);
+		mQAC.makeAuthoredQAvailOffline();
+		
+		OfflineData model = new OfflineData();
+		OfflineDataController controller = new OfflineDataController(model);
+		
+		ArrayList<Question> expected = new ArrayList<Question>();
+		expected.add(mQ1);
+		
+		assertEquals("Testing if authored questions are made"
+				+ "available offline",expected,controller.getOfflineAuthoredQs());
+		
+	}
 }
