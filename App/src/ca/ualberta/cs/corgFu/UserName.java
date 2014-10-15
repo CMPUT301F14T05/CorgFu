cfce59d9fd9ba21e187f44b216d87b75f534467f
wrflemin <wrflemin@ualberta.ca>
2014-10-15 15:22:21 -0600

    Added Wyatt's final test case for #UC-22 (SetUsername) and added empty class for representing the username.

---
+++ App/src/ca/ualberta/cs/corgFu/UserName.java
@@ -0,0 +1,27 @@
+package ca.ualberta.cs.corgFu;
+
+public class UserName {
+
+	private String currUserName;
+	
+	public UserName() {
+
+	}
+	
+	public void saveUserName(){
+		
+	}
+
+	public UserName loadUserName(){
+		return null;
+	}
+	
+	public String getUserName(){
+			return "";
+	}
+	
+	public void setUserName(String username){
+		
+	}
+	
+}
--- CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/SetUsernameTest.java
+++ CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/SetUsernameTest.java
@@ -1,7 +1,35 @@
 package ca.ualberta.cs.corgfuapp.test;
 
 import junit.framework.TestCase;
+import ca.ualberta.cs.corgFu.UserName;
 
 public class SetUsernameTest extends TestCase {
 	
+	public SetUsernameTest(){
+		super();
+	}
+	
+	public void testInitializeUsername(){
+		UserName myUserName = new UserName();
+		assertNotNull(myUserName);
+	}
+	
+	public void testSetUsername(){
+		String firstUsername = "testUser";
+		String secondUsername = "second testUser";
+		UserName myUserName = new UserName();
+		myUserName.setUserName(firstUsername);
+		assertEquals("Testing setting username", firstUsername, myUserName.getUserName());
+		myUserName.setUserName(secondUsername);
+		assertEquals("Testing setting another username", secondUsername, myUserName.getUserName());
+	}
+	public void testLoadSaveUsername(){
+		String testUsername = "testUser";
+		UserName myUserName = new UserName();
+		myUserName.setUserName(testUsername);
+		
+		myUserName.saveUserName();
+		
+		assertEquals("Testing load/save username", testUsername, myUserName.loadUserName().getUserName());
+	}
 }
