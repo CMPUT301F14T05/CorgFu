935feb62ee7023d8a0bf6f14a3f4f8ea6d701e92
ajwu <ajwu@ualberta.ca>
2014-10-16 14:35:21 -0600

    completed UC-09

--- CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/SmallPicturesOnlyTest.java
+++ CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/SmallPicturesOnlyTest.java
@@ -1,7 +1,17 @@
 package ca.ualberta.cs.corgfuapp.test;
 
+import ca.ualberta.cs.corgFu.Picture;
 import junit.framework.TestCase;
+import android.graphics.Bitmap;
 
 public class SmallPicturesOnlyTest extends TestCase {
-
+	
+	public void testSmallPicturesOnly(){
+		Bitmap bigBMP = BogoPicGen.generateBitmap(400, 400);
+		Bitmap smallBMP = BogoPicGen.generateBitmap(200, 200);
+		Picture testPic = new Picture(); 	
+		
+		assertTrue("Testing if attached picture requirements; bigBMP returns False", testPic.smallPicture(bigBMP));
+		assertTrue("Testing if attached picture requirements; smallBMP returns False", testPic.smallPicture(smallBMP));
+	}
 }
