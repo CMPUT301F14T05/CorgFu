d853a533f7e593dca29e62721341b06fe11837f4
wrflemin <wrflemin@ualberta.ca>
2014-10-17 17:35:11 -0600

    Added empty Picture class so test code for small pictures only would compile

---
+++ App/src/ca/ualberta/cs/corgFu/Picture.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.corgFu;
+
+import android.graphics.Bitmap;
+
+public class Picture {
+
+	public Picture() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean smallPicture(Bitmap bigBMP) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
