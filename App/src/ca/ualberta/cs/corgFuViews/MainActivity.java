3b681d5c88a7070dd0710829fbf9d5ab96d09d01
ajwu <ajwu@ualberta.ca>
2014-10-25 15:08:36 -0600

    Added attachPicture View for testing

--- App/src/ca/ualberta/cs/corgFuViews/MainActivity.java
+++ App/src/ca/ualberta/cs/corgFuViews/MainActivity.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.corgFuViews;
 import ca.ualberta.corgfuapp.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,7 +21,6 @@ public class MainActivity extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
@@ -30,11 +30,21 @@ public class MainActivity extends Activity
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+		case R.id.attach_picture:
+			Intent attachPicture = new Intent(MainActivity.this,AttachPicture.class);
+			startActivity(attachPicture);
+		finish();
+		return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+		
+		
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
 	}
-
 }
