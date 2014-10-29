7945b05bc045166a294fda503aa940e6d8ed9bd2
shevaroller <shevaroller@gmail.com>
2014-10-29 16:58:49 -0600

    Updated Picture class

--- App/src/ca/ualberta/cs/corgFu/Picture.java
+++ App/src/ca/ualberta/cs/corgFu/Picture.java
@@ -8,10 +8,14 @@ public class Picture {
 	// max size of the picture in bytes
 	private static int MAX_PICTURE_SIZE = 65536;
 	
+	private int pictureSize;
+	
+	//private Bitmap image;
+	
 	// via an intent in VIEW 
 	// attachImage method modifies Model attributes "hasPic" 
 	// hasPic = True means Image successfully attached; vice versa if otherwise
-	public void attachImageQuest(Question Quest, Bitmap Image){
+	public void attachImageQuest(Question question, Bitmap image) {
 	  /* 1. fetches picture from camera directory 
 			
 		 2. fetches picture from another directory
@@ -19,24 +23,28 @@ public class Picture {
 		 3. prescribe a default image if no images are supplied/option aborted */
 		
 		// Finally, attach Image meets requirements 
-		if (smallPicture(Image)){
-			Quest.setImage(Image);
+		if (smallPicture(image)) {
+			question.setImage(image);
 		}
 	}
 
-	public void attachImageAns(Answer Ans, Bitmap Image){
-		if (smallPicture(Image)){
+	public void attachImageAns(Answer Ans, Bitmap Image) {
+		if (smallPicture(Image)) {
 			Ans.setPicture(Image);
 		}
 	}
 	
-	public Boolean smallPicture(Bitmap Image){
+	public Boolean smallPicture(Bitmap Image) {
 		// attached picture meets requirements
-		if (byteSizeOf(Image) <= MAX_PICTURE_SIZE){
+		if (byteSizeOf(Image) <= MAX_PICTURE_SIZE) {
 			return true;
 		}
 		// attached picture fails requirements 
 		return false;
+	}
+	
+	public int getPictureSize(Picture picture) {
+		return picture.pictureSize;
 	}
 	
 	public int byteSizeOf(Bitmap bitmap) {
