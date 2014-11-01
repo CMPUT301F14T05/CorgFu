1a8a47f07a7bde385dc260660ca6673c9ff6619f
sensible-heart <alexandra.makepeace@gmail.com>
2014-11-01 14:59:52 -0600

    Partial implementation of adding questions to our server

--- App/src/ca/ualberta/cs/corgFuModels/AllQuestions.java
+++ App/src/ca/ualberta/cs/corgFuModels/AllQuestions.java
@@ -2,10 +2,14 @@ package ca.ualberta.cs.corgFuModels;
 
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
 import com.google.gson.Gson;
 import ca.ualberta.cs.corgFu.IView;
 import ca.ualberta.cs.corgFu.Model;
@@ -15,10 +19,13 @@ public class AllQuestions extends Model<IView>
 {
 	private ArrayList<Question> allQuestions;
 	private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301f14t05/";
+	private static final String TAG = "QuestionSearch";
+	
 	private Gson gson;
 	
 	public AllQuestions(){
 		allQuestions = new ArrayList<Question>();
+		gson = new Gson();
 		
 	}
 	
@@ -27,15 +34,15 @@ public class AllQuestions extends Model<IView>
 		HttpClient httpClient = new DefaultHttpClient();
 
 		try {
-//			HttpPost addRequest = new HttpPost(RESOURCE_URL + movie.getId());
-//
-//			StringEntity stringEntity = new StringEntity(gson.toJson(movie));
-//			addRequest.setEntity(stringEntity);
-//			addRequest.setHeader("Accept", "application/json");
-//
-//			HttpResponse response = httpClient.execute(addRequest);
-//			String status = response.getStatusLine().toString();
-//			Log.i(TAG, status);
+			HttpPost addRequest = new HttpPost(RESOURCE_URL + Q.getQuestionString());
+
+			StringEntity stringEntity = new StringEntity(gson.toJson(Q));
+			addRequest.setEntity(stringEntity);
+			addRequest.setHeader("Accept", "application/json");
+
+			HttpResponse response = httpClient.execute(addRequest);
+			String status = response.getStatusLine().toString();
+			Log.i(TAG, status);
 
 		} catch (Exception e) {
 			e.printStackTrace();
