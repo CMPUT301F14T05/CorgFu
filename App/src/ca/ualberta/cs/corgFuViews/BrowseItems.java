e154fb21f8bc99ccba79d6cdbda6ba87e8462c28
dbsigurd <dbsigurd@ualberta.ca>
2014-11-02 15:05:35 -0700

    fixed format

--- App/src/ca/ualberta/cs/corgFuViews/BrowseItems.java
+++ App/src/ca/ualberta/cs/corgFuViews/BrowseItems.java
@@ -4,145 +4,196 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 import ca.ualberta.corgfuapp.R;
 import ca.ualberta.cs.corgFu.AllQuestionsApplication;
-import ca.ualberta.cs.corgFu.InsertQuestionAdapter;
 import ca.ualberta.cs.corgFu.IView;
+import ca.ualberta.cs.corgFu.InsertQuestionAdapter;
 import ca.ualberta.cs.corgFuControllers.AllQuestionsController;
 import ca.ualberta.cs.corgFuModels.AllQuestions;
 import ca.ualberta.cs.corgFuModels.Question;
 
-public class BrowseItems extends Activity implements IView, OnItemSelectedListener {
+public class BrowseItems extends Activity implements IView
+{
 	
 	private InsertQuestionAdapter listAdapter;
 	Spinner sortOptions;
-	
+	AllQuestions AQ;
+	AllQuestionsController AQController;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
+
 		super.onCreate(savedInstanceState);
+		//calls function to populate list
+		populateListView();
+
+	}
+
+	// Taken from
+	// http://developer.android.com/guide/topics/ui/controls/spinner.html
+
+	// populates list view using adapter class
+	public void populateListView()
+	{
+
 		setContentView(R.layout.activity_browse_items);
-		
 		AllQuestions AQ = AllQuestionsApplication.getAllQuestions();
 		AQ.addView(this);
-		
-		AllQuestionsController AQController = AllQuestionsApplication.getAllQuestionsController();
-		//sets up arrayAdapter for the listview.
-		//Not sure if This format should be used, because questions might not update.
-		listAdapter = new InsertQuestionAdapter(this, AQController.sortByDate());
-		ListView bQListView = (ListView) findViewById(R.id.browseQuestionsListView);
-		bQListView.setAdapter(listAdapter);
-		
-		//Taken from http://developer.android.com/guide/topics/ui/controls/spinner.html
-		//Populates spinner (dropdown menu)
-		Spinner sortOptions = (Spinner) findViewById(R.id.sortSpinner);
-		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, 
-				R.array.sortOptions, android.R.layout.simple_spinner_item);
-		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		final AllQuestionsController AQController = AllQuestionsApplication
+				.getAllQuestionsController();
+
+		// populateSpinner(0);
+		// Toast.makeText(this,"chose sort date", Toast.LENGTH_SHORT).show();
+		final Spinner sortOptions = (Spinner) findViewById(R.id.sortSpinner);
+		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter
+				.createFromResource(this, R.array.sortOptions,
+						android.R.layout.simple_spinner_item);
+		spinnerAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		sortOptions.setAdapter(spinnerAdapter);
-		sortOptions.setOnItemSelectedListener(this);
-		
-		
-		Button myProfileButton = (Button)findViewById(R.id.MyProfileButton);
-		Button askButton = (Button)findViewById(R.id.GoToAsk);
-		
-	}
-	@Override
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-			long arg3)
-	{
-		int position = sortOptions.getSelectedItemPosition();
-		switch(position){
-			case 0:
-				Toast.makeText(this,"chose sort date", Toast.LENGTH_SHORT).show();
-				break;
-			case 1:
-				Toast.makeText(this,"chose sort date", Toast.LENGTH_SHORT).show();
-				break;
-			case 2:
-				Toast.makeText(this,"chose sort date", Toast.LENGTH_SHORT).show();
-				break;
-				
-		}
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0)
-	{
+		// creates a listener to determine sort order
+		sortOptions.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
 
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3)
+			{
+
+				int position = sortOptions.getSelectedItemPosition();
+				ListView bQListView = (ListView) findViewById(R.id.browseQuestionsListView);
+				switch (position)
+				{
+				// will sort by date
+					case 0:
+						System.out.println("chose0");
+						listAdapter = new InsertQuestionAdapter(
+								BrowseItems.this, AQController.sortByDate());
+						bQListView = (ListView) findViewById(R.id.browseQuestionsListView);
+						bQListView.setAdapter(listAdapter);
+
+						// Toast.makeText(this,"chose sort date",
+						// Toast.LENGTH_SHORT).show();
+						break;
+					// will sort by pic
+					case 1:
+						System.out.println("chose1");
+						listAdapter = new InsertQuestionAdapter(
+								BrowseItems.this, AQController.sortByPicture());
+						bQListView = (ListView) findViewById(R.id.browseQuestionsListView);
+						bQListView.setAdapter(listAdapter);
+						// Toast.makeText(this,"chose sort date",
+						// Toast.LENGTH_SHORT).show();
+						break;
+					// will sort by vote
+					case 2:
+						System.out.println("chose2");
+
+						listAdapter = new InsertQuestionAdapter(
+								BrowseItems.this, AQController.sortByUpvote());
+						bQListView = (ListView) findViewById(R.id.browseQuestionsListView);
+						bQListView.setAdapter(listAdapter);
+						// Toast.makeText(this,"chose sort date",
+						// Toast.LENGTH_SHORT).show();
+						break;
+
+				}
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0)
+			{
+
+				// TODO Auto-generated method stub
+
+			}
+		});
+		Button myProfileButton = (Button) findViewById(R.id.MyProfileButton);
+		Button askButton = (Button) findViewById(R.id.GoToAsk);
+
 	}
 	
 	@Override
-	public void onDestroy(){
+	public void onDestroy()
+	{
+
 		super.onDestroy();
 		AllQuestions aQ = AllQuestionsApplication.getAllQuestions();
 		aQ.deleteView(this);
 	}
-	
-	
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.browse_items, menu);
 		return true;
 	}
 
-	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
 
-	public void toAskQuestion(View view){
-		Intent intent = new Intent(this,MainActivity.class);
-    	startActivity(intent);
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings)
+		{
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
-	
-	public void toUserProfile(View view){
-		
+
+	public void toAskQuestion(View view)
+	{
+
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
 	}
-	
-	public void viewMostUpvotedQuestions(){
-		//Test string
+
+	public void toUserProfile(View view)
+	{
+
 	}
-	
-	public void viewMostUpvotedAnswers(){
-		
+
+	public void viewMostUpvotedQuestions()
+	{
+
+		// Test string
+	}
+
+	public void viewMostUpvotedAnswers()
+	{
+
 	}
 
 	public static ArrayList<Question> getCurrentDisplayCollection()
 	{
-		return null;	
+
+		return null;
 	}
 
 	@Override
-	public void update(Object model) {
+	public void update(Object model)
+	{
+
 		listAdapter.notifyDataSetChanged();
 	}
 
-	
 }
