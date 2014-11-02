41fbc4035ebce0dfa894037b0c7a51d7e485c04f
dbsigurd <dbsigurd@ualberta.ca>
2014-11-01 20:25:14 -0600

    work in progress

--- App/src/ca/ualberta/cs/corgFuControllers/AllQuestionsController.java
+++ App/src/ca/ualberta/cs/corgFuControllers/AllQuestionsController.java
@@ -1,7 +1,6 @@
 package ca.ualberta.cs.corgFuControllers;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -11,18 +10,42 @@ import ca.ualberta.cs.corgFuModels.Question;
 public class AllQuestionsController {
 
 	AllQuestions allQuestions;
-	Comparator<Question> dateSorter = null;
+	ArrayList<Question> allQuestionsArray;
+	//Comparator<Question> dateSorter = null;
 	
 	public AllQuestionsController(AllQuestions aQ) {
 		allQuestions = aQ;
-		//makes a comparator for sorting questions by date
-		dateSorter = new Comparator<Question>(){
-			@Override
-			public int compare(Question Q1, Question Q2){
-				return Q1.getDate().compareTo(Q2.getDate());
-			}
-		};
+		ArrayList<Question> allQuestionsArray = allQuestions.getAllQuestions();
 		
+	}
+
+	public class DateComparator implements Comparator<Question>{
+		@Override
+		public int compare(Question lhs, Question rhs) {
+			return lhs.getDate().compareTo(rhs.getDate());
+		}
+		
+	}
+	public ArrayList<Question> sortByDate(){
+		for (Question question:allQuestionsArray){
+			System.out.println(question.getQuestionString());
+		}
+		
+		Collections.sort(allQuestionsArray, new DateComparator());
+		for (Question question:allQuestionsArray){
+			System.out.println(question.getQuestionString());
+		}
+		return allQuestionsArray;
+	}
+	
+	public ArrayList<Question> sortByPicture(){
+		/*
+		 * sort date
+		 * then sort if has picture
+		 * 
+		 * if the question in array has a picture
+		 */
+		return null;
 	}
 
 	public ArrayList<Question> sortByUpvote() {
@@ -38,14 +61,9 @@ public class AllQuestionsController {
 	public ArrayList<Question> getAllQuestions(){
 		return allQuestions.getAllQuestions();
 	}
-	public ArrayList<Question> sortByDate(){
-		Collections.sort(allQuestions.getAllQuestions(), dateSorter);
-		return allQuestions.getAllQuestions();
-	}
 	
-	public ArrayList<Question> sortByPicture(){
-		return null;
-	}
+	
+	
 	
 	public void addQuestion(Question Q){
 		allQuestions.addQuestion(Q);
--- CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/SortQuestionsDateTest.java
+++ CorgFuAppTestingTest/src/ca/ualberta/cs/corgfuapp/test/SortQuestionsDateTest.java
@@ -1,30 +1,67 @@
 package ca.ualberta.cs.corgfuapp.test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+import junit.framework.TestCase;
 import ca.ualberta.cs.corgFuControllers.AllQuestionsController;
 import ca.ualberta.cs.corgFuModels.AllQuestions;
 import ca.ualberta.cs.corgFuModels.Question;
-import junit.framework.TestCase;
 
 public class SortQuestionsDateTest extends TestCase {
+	AllQuestions allQuestions;
+	ArrayList<Question> allQuestionsArray;
+	public class DateComparator implements Comparator<Question>{
+		@Override
+		public int compare(Question lhs, Question rhs) {
+			return lhs.getDate().compareTo(rhs.getDate());
+		}
+		
+	}
 	
 	public void testViewHasCorrectDateOrder(){
 		Question mQ1 = new Question("Question 1");//add question text
 		Question mQ2 = new Question("Question 2");//add question text
-		
-		AllQuestions mAQ = new AllQuestions();
-		mAQ.addQuestion(mQ1);
-		mAQ.addQuestion(mQ2);
-		
-		AllQuestionsController mQC = new AllQuestionsController(mAQ);
+		Question mQ3 = new Question("Question 3");//add question text
+//		AllQuestions mAQ = new AllQuestions();
+//		mAQ.addQuestion(mQ1);
+//		mAQ.addQuestion(mQ3);
+//		mAQ.addQuestion(mQ2);
+		ArrayList<Question> sortQList = new ArrayList<Question>();
+		//AllQuestionsController mQC = new AllQuestionsController(mAQ);
 		
 		// expected output should arrange questions by most recent to latest according to date  
 		ArrayList<Question> expected = new ArrayList<Question>();
 		expected.add(mQ1);
 		expected.add(mQ2);
+		expected.add(mQ3);
+		sortQList.add(mQ1);
+		sortQList.add(mQ3);
+		sortQList.add(mQ2);
+		for (Question question:sortQList){
+			System.out.println(question.getQuestionString());
+		}
+		Collections.sort(sortQList, new DateComparator() {
+			public int compare(Question lhs, Question rhs) {
+				if(lhs.getDate().before(rhs.getDate())){
+					return 1;
+				}else if (lhs.getDate().after(rhs.getDate())){
+					return -1;
+				}
+				return 0;
+				}
+			}
+		);
+		for (Question l2:sortQList){
+			System.out.println(l2.getQuestionString());
+		}
+		/*sortQList = mQC.sortByDate();
+		for (Question question:sortQList){
+				System.out.println(question.getQuestionString());
+		}*/
+		assertEquals("Testing if QuestionController returns list in correct date order", 
+				expected,sortQList);
 		
-		assertEquals("Testing if QuestionController returns list in correct date order", mQC.sortByDate()
-				,expected);
 	}
 }
