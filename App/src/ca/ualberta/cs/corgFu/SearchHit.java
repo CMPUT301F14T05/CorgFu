c2ff546a3b6b305cadc2b4917aa741f6c4eb20c4
sensible-heart <alexandra.makepeace@gmail.com>
2014-11-06 11:37:24 -0700

    Finished the Javadoc information for the SearchHit class

--- App/src/ca/ualberta/cs/corgFu/SearchHit.java
+++ App/src/ca/ualberta/cs/corgFu/SearchHit.java
@@ -1,9 +1,19 @@
 package ca.ualberta.cs.corgFu;
+/**This class SearchHit is used to determine whether or not a Question has been successfully found in a search.
+ * 
+ * @author Alex Makepeace
+ * 
+ * @version 1.0 Nov.6/2014
+ */
 
 import ca.ualberta.cs.corgFuModels.Question;
 
 public class SearchHit<T> {
-
+	
+	/**Finds out the source question that the search hit has found
+	 * 
+	 * @return the Question that has been attached to the SearchHit
+	 */
 	public Question getSource() {
 		// TODO Auto-generated method stub
 		return null;
