f75c72a6b8d5bda36169ed54a3883f66caabf406
ajwu <ajwu@ualberta.ca>
2014-11-18 16:09:13 -0700

    ExpandableAdapter implementation

--- App/src/ca/ualberta/cs/corgFu/InsertAnswerAdapter.java
+++ App/src/ca/ualberta/cs/corgFu/InsertAnswerAdapter.java
@@ -1,73 +1,106 @@
 package ca.ualberta.cs.corgFu;
 
+import java.util.HashMap;
+import java.util.List;
+
+import ca.ualberta.corgfuapp.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
  
 public class InsertAnswerAdapter extends BaseExpandableListAdapter {
-
-	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public View getChildView(int groupPosition, int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Object getGroup(int groupPosition) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getGroupCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public long getGroupId(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	 
+    private Context _context;
+    private List<String> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<String>> _listDataChild;
  
-
+    public InsertAnswerAdapter(Context context, List<String> listDataHeader,
+            HashMap<String, List<String>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+ 
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+ 
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+ 
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+            boolean isLastChild, View convertView, ViewGroup parent) {
+ 
+        final String childText = (String) getChild(groupPosition, childPosition);
+ 
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.reply_item, null);
+        }
+ 
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.replyItem);
+ 
+        txtListChild.setText(childText);
+        return convertView;
+    }
+ 
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+ 
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+ 
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+ 
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+ 
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+            View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.answer_header, null);
+        }
+ 
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.answerHeader);
+        lblListHeader.setText(headerTitle);
+ 
+        return convertView;
+    }
+ 
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+ 
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
 }
-
