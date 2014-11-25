85bc28c06ba44fc93199c2db7f1e740969a58b23
shevaroller <shevaroller@gmail.com>
2014-11-24 19:16:04 -0700

    Setting up question image processing in MainActivity

--- App/src/ca/ualberta/cs/corgFuViews/MainActivity.java
+++ App/src/ca/ualberta/cs/corgFuViews/MainActivity.java
@@ -1,12 +1,20 @@
 package ca.ualberta.cs.corgFuViews;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Images.Media;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +25,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import ca.ualberta.corgfuapp.R;
 import ca.ualberta.cs.corgFu.AllQuestionsApplication;
+import ca.ualberta.cs.corgFu.Picture;
 import ca.ualberta.cs.corgFuControllers.AllQuestionsController;
 import ca.ualberta.cs.corgFuControllers.DataController;
 import ca.ualberta.cs.corgFuModels.Question;
@@ -33,10 +42,16 @@ import ca.ualberta.cs.corgFuModels.Question;
  */
 public class MainActivity extends Activity
 {
+	/**
+	 * RESULT_LOAD_IMAGE indicates whether an Image has been successfully loaded
+	 */
+	private static int RESULT_LOAD_IMAGE = 111;
+	
 	public static Context context;
 	/** This is the question that is being added by the user*/
 	protected Question q; //most recent Question added by the user
 	DataController DC;
+	
 	/**onCreate() sets up the display of the Activity, it makes sure that all of the desired text is set to the correct typeface.*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -111,7 +126,10 @@ public class MainActivity extends Activity
         builder.setPositiveButton(R.string.yes_button_text, new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
                 	   // Go to another activity that fetches pictures from Android Media
-                	   //addPicture();
+                       // User wants to add a picture, fetch it from Image Gallery
+                 	   //Intent i = new Intent(Intent.ACTION_PICK, Media.EXTERNAL_CONTENT_URI);
+
+                 	   //startActivityForResult(i, RESULT_LOAD_IMAGE);
                    }
 
                });
@@ -126,6 +144,61 @@ public class MainActivity extends Activity
 	    builder.show();
 		
 	}
+	
+    /**
+     *  Function onActivityResult expects requestCode of successfully loaded image
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+
+    	//fetches picture from image directory  
+    	if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && null != data) {
+
+    		Uri selectedImage = data.getData();
+    		InputStream is = null;
+    		Bitmap attachedPic = null;
+			try {
+				is = this.getContentResolver().openInputStream(selectedImage);
+				attachedPic = BitmapFactory.decodeStream(is);
+	    		
+			} catch (FileNotFoundException e) {
+				// Invalid URI exception
+				e.printStackTrace();
+			}
+
+			try {
+				is.close();
+			} catch (IOException e) {
+				// Attempt to close non-existing InputStream
+				e.printStackTrace();
+			}
+
+    		if (Picture.smallPicture(attachedPic)) {
+    			// Add image to the question
+    			Toast.makeText(this, "Picture is added", Toast.LENGTH_SHORT).show();
+    			//((MainActivity)getActivity()).q.setImage(attachedPic);
+    			//AllQuestionsController AQC = AllQuestionsApplication.getAllQuestionsController();
+    			// Pick the most recently added question
+    			//Question q = AQC.getRecentQuestion();
+    			//q.setImage(attachedPic);
+    			
+    			// After picture is added go back to MainActivity
+    			Intent i = new Intent(this, MainActivity.class);
+    			//i.putExtra(name, value)
+    			startActivity(i);
+    		}
+    		else {
+    			Toast.makeText(this, "image is too large", Toast.LENGTH_SHORT).show();
+    			// Image is too large. Invoke another dialog asking to add another image
+    			AddSmallerPictureDialog addPictureDialog = new AddSmallerPictureDialog();
+    			addPictureDialog.show(getFragmentManager(), null);
+    			
+    			setContentView(R.layout.activity_add_picture);
+
+    		}
+
+    	}
+    }
 	
 	/**onCreateOptionsMenu() is an auto-generated method */
 	@Override
