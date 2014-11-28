ac688fdb2d58321ef8ec9b3f95dfbfea631c8289
dbsigurd <dbsigurd@ualberta.ca>
2014-11-27 21:28:42 -0700

    fixed saving

--- App/src/ca/ualberta/cs/corgFu/DataManager.java
+++ App/src/ca/ualberta/cs/corgFu/DataManager.java
@@ -1,21 +1,26 @@
 package ca.ualberta.cs.corgFu;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
 import java.io.StreamCorruptedException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import android.content.Context;
 import android.util.Log;
 import ca.ualberta.cs.corgFuModels.OfflineData;
 import ca.ualberta.cs.corgFuModels.Question;
-import ca.ualberta.cs.corgFuViews.LoginActivity;
 import ca.ualberta.cs.corgFuViews.MainActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class DataManager {
 	private static final String FavouritesFile = "Favourites.save";
@@ -49,16 +54,14 @@ public class DataManager {
 		Log.i(saveString,"start");
 		try {
 			Log.i(saveString, "fail1");
-			FileOutputStream fos =null;
-			
-			fos =  context.openFileOutput(choice,Context.MODE_PRIVATE);
-			
+			FileOutputStream fos =  context.openFileOutput(choice,Context.MODE_PRIVATE);
+			Gson gson = new Gson();
 			Log.i(saveString, "fail2");
-			ObjectOutputStream osw = new ObjectOutputStream(fos);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
 			Log.i(saveString, "fail4");
-			osw.writeObject(dataList);
+			gson.toJson(dataList,osw);
 			Log.i(saveString, "fail5");
-			osw.close();
+			osw.flush();
 			Log.i(saveString, "fail6");
 			fos.close();
 			
@@ -73,7 +76,6 @@ public class DataManager {
 		
 	
 	// load based on choice 0 = fav, 1 =cache, 2= later
-	@SuppressWarnings("unchecked")
 	public ArrayList<Question> loadFavouritesFromFile(String choice){
 		try{
 			File fh = new File(context.getFilesDir(), choice);
@@ -85,16 +87,17 @@ public class DataManager {
 				return aq;
 			}
 			FileInputStream fis = context.openFileInput(choice);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
 			
 			Log.i("Loading", "load2");
-			ObjectInputStream in;
-			
-			in = new ObjectInputStream(fis);
+			Gson gson = new Gson();
 		
 			Log.i("Loading", "load3");
+			Type listType = new TypeToken<ArrayList<Question>>(){}.getType();
 			//favourites = Favourites.getInstance();
 			Log.i("Loading", "load4");
-			favouriteList = (ArrayList<Question>) in.readObject();
+			favouriteList = gson.fromJson(in, listType);
 			Log.i("Loading", "load5");
 			
 			
@@ -110,9 +113,7 @@ public class DataManager {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (ClassNotFoundException e){
-			e.printStackTrace();
-		}
+		} 
 		return favouriteList;
 	}
 	
